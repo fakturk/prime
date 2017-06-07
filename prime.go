b4c2b2043275ad302f0adf5412a8f2d467ab3e12
Fatih Akturk <f.akturk@gmail.com>
2017-06-07 16:54:21 +0900

    Definition of function
    Definition of function IsPrime added

--- prime.go
+++ prime.go
@@ -1,19 +1,21 @@
 // Package prime cheks if the the given number is prime
 package prime
+
 import "math"
 
+// IsPrime checks the given number is prime or not, returns boolean
 func IsPrime(number int) bool {
-  prime:=false
-  if number==3||number==2 {
-    prime=true
-  }
-  for i := 2; i <= int(math.Sqrt(float64(number))); i++ {
-    // fmt.Println(i)
-    if number%i==0 {
-      prime=false
-      break
-    }
-    prime=true
-  }
-  return prime
+	prime := false
+	if number == 3 || number == 2 {
+		prime = true
+	}
+	for i := 2; i <= int(math.Sqrt(float64(number))); i++ {
+
+		if number%i == 0 {
+			prime = false
+			break
+		}
+		prime = true
+	}
+	return prime
 }
